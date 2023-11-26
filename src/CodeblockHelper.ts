72ba92120b4a84a413dd875812d298bd948f8c85
Maarten Staa <maartens@spotify.com>
2023-11-26 13:59:14 +0100

    fix: ignore mentions in inline code blocks

--- src/CodeblockHelper.ts
+++ src/CodeblockHelper.ts
@@ -1,11 +1,12 @@
-import { CODEBLOCK_REG_EXP } from './RegExp';
+import { CODEBLOCK_REG_EXP, CODE_INLINE_REG_EXP } from './RegExp';
 
-export function getCodeblockPositions(text: string, startIndex: number = 0): [from: number, to: number][] {
-    const codeblocks = [...text.matchAll(CODEBLOCK_REG_EXP)];
-    let codeblockPositions: [from: number, to: number][] = [];
+type CodeblockRange = [from: number, to: number];
+
+export function getCodeblockPositions(text: string, startIndex: number = 0): CodeblockRange[] {
+    const codeblocks = [...text.matchAll(CODEBLOCK_REG_EXP), ...text.matchAll(CODE_INLINE_REG_EXP)];
     return codeblocks.map((c) => [startIndex + c.index, startIndex + c.index + c[0].length]);
 }
 
-export function isPositionInCodeblock(codeblocks: [from: number, to: number][], position: number) {
+export function isPositionInCodeblock(codeblocks: CodeblockRange[], position: number) {
     return codeblocks.find((c) => c[0] < position && c[1] > position) != null;
 }
